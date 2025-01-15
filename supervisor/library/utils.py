0b276746b0f36737979bd1b0bf61f67f79d0c288
root <ky@chtlab.io>
2025-01-15 11:56:49 +0800

    logging

--- supervisor/library/utils.py
+++ supervisor/library/utils.py
@@ -32,7 +32,8 @@ class Utils:
     
     def get_states(self, url, headers):        
         try:
-            response = requests.get(url, headers=headers)
+            print(headers)
+            response = requests.get('http://192.168.31.18:8123/api/states', headers=headers)
             response.raise_for_status()  # Raises an exception for HTTP error codes
             return response.json()
         except requests.exceptions.RequestException as e:
